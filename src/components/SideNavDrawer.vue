1c2edf432d5cba10516742265cd5b0864c806e45
Nicholas Ferrara <nick.d.ferrara@gmail.com>
2024-10-21 22:39:30 -0400

    Refactor hover effect for nav drawer

--- src/components/SideNavDrawer.vue
+++ src/components/SideNavDrawer.vue
@@ -5,7 +5,8 @@
         v-for="item in items"
         :key="item.title"
         :to="item.route"
-        class="py-1"
+        class="py-1 my-1 mx-2 rounded-lg"
+        :class="{ 'selected-item': isSelected(item.route) }"
       >
         <v-list-item-content class="py-0">
           <v-list-item-title class="d-flex align-center">
@@ -19,6 +20,11 @@
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute();
+
 const items = [
   { title: 'Home', icon: 'mdi-home', route: '/' },
   { title: 'Products', icon: 'mdi-package-variant', route: '/products' },
@@ -26,7 +32,11 @@ const items = [
   { title: 'Picking', icon: 'mdi-hand-pointing-right', route: '/picking' },
   { title: 'Shipping', icon: 'mdi-truck', route: '/shipping' },
   { title: 'Invoices', icon: 'mdi-file-document', route: '/invoices' },
-]
+];
+
+const isSelected = computed(() => (itemRoute: string) => {
+  return route.path === itemRoute;
+});
 </script>
 
 <style scoped>
@@ -36,5 +46,14 @@ const items = [
 
 .v-list-item-title {
   font-size: 0.875rem;
+}
+
+.selected-item {
+  background-color: var(--v-primary-base);
+  color: white;
+}
+
+.selected-item:hover {
+  background-color: var(--v-primary-darken1);
 }
 </style>
